70c73440bf67682064913912b25d8e6d7357b89f
John Simiyu <jonnyeinstein@gmail.com>
2018-12-20 23:42:45 +0300

    Updated classes

--- src/config/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-var config = require('./config.js');
-
-module.exports.config = {
-    config
-};
--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var oauth_token = require('./oauth_token.js');
-var oauth = require('./oauth.js');
-
-module.exports.util = async function(){
-    oauth_token;
-    oauth;
-};
